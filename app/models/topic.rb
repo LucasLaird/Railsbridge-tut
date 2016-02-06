16bf4adda01dd2338d5834a37488b6b316db58ce
Lucas Laird <lucas.laird@colorado.edu>
2016-02-06 14:46:28 -0700

    Create downvote and upvote in model. Refactor WIP

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,10 @@
 class Topic < ActiveRecord::Base
 	has_many :votes, dependent: :destroy
+	def upvote(topic)
+		topic.votes.create
+    end
+
+    def downvote(topic)
+   		topic.votes.last.destroy if topic.votes.present?
+	end
 end
